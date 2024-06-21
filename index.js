c1f0b06c10469b8cfbba920096f747f94920aaf9
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-06-21 09:56:27 +0200

    Add operator logic

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var num1st;
+var num2nd;
+var operator;
+
 function add(num1, num2){
     return num1 + num2;
 }
@@ -12,4 +16,20 @@ function multiply(num1, num2){
 
 function divide(num1, num2){
     return num1 / num2;
+}
+
+function operate(a, op, b){
+    switch(op){
+        case "add":
+            return add(a, b);
+        case "subtract":
+            return subtract(a, b);
+        case "multiply":
+            return multiply(a, b);
+        case "divide":
+            return divide(a, b);
+        default:
+            console.log("Unknown operator");
+            break;
+    }
 }
